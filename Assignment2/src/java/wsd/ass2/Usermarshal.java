63ef86840a65e3138a532f8081f86927ea32a180
jmhuggart <jmhuggart@gmail.com>
2016-10-03 15:57:12 +1100

    Add files via upload
    + Usermarshal.java
        - Added to allow for register of users (add new entries to users.xml)
    
    + Users.java
        - Updated with method emailInUse
        - This facilitates register of users by ensuring there are no duplicate email addresses

---
+++ Assignment2/src/java/wsd/ass2/Usermarshal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsd.ass2;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+/**
+ *
+ * @author Jack Huggart
+ */
+public class Usermarshal {
+    public static void marshal(String filepath, Users users) throws JAXBException, FileNotFoundException {
+        JAXBContext jc = JAXBContext.newInstance(Users.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        
+        m.marshal(users, new FileOutputStream(filepath));
+    }
+}
---
+++ Assignment2/src/java/wsd/ass2/Users.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsd.ass2;
+
+import java.io.*;
+import java.util.*;
+import javax.xml.bind.annotation.*;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "users")
+public class Users implements Serializable {
+
+    @XmlElement(name = "user")
+    private ArrayList<User> list = new ArrayList<User>();
+    
+    public Users() {
+    }
+ 
+    public ArrayList<User> getList() {
+        return list;
+    }
+    public void addUser(User user) {
+        list.add(user);
+    }
+    public void removeUser(User user) {
+        list.remove(user);
+    }
+    public User login(String email, String password) {
+        // For each user in the list...
+        for (User user : list) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password))
+                return user; // Login correct. Return this user.
+        }
+        return null; // Login incorrect. Return null.
+    }   
+    
+    public boolean emailInUse(String email) {
+        for (User user : list) {
+            if (email.equals(user.getEmail())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
