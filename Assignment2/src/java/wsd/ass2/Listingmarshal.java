a588be14c63725a6d874943aed8dc7b11f93fd57
jmhuggart <jmhuggart@gmail.com>
2016-10-07 15:10:26 +1100

    Add files via upload
    + Enquirymarshal.java, Listingmarshal.java
        - Marshallers for the two other xml documents

---
+++ Assignment2/src/java/wsd/ass2/Listingmarshal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsd.ass2;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+/**
+ *
+ * @author Jack Huggart
+ */
+public class Listingmarshal {
+    public static void marshal(String filepath, Listings listings) throws JAXBException, FileNotFoundException {
+        JAXBContext jc = JAXBContext.newInstance(Listings.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        
+        m.marshal(listings, new FileOutputStream(filepath));
+    }
+}
