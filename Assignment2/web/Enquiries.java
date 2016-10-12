9c982dcc13f33c92241a8baca2446658200b488f
jmhuggart <jmhuggart@gmail.com>
2016-10-12 14:28:15 +1100

    Add files via upload
    + enquiries.xsd, enquiries.xml
        - Incorrect namespaces broke website, fixed and tested
    
    + Enquiries.java
        - Added namespace binding to root element enquiries to reflect changes i above files

---
+++ Assignment2/web/Enquiries.java
@@ -0,0 +1,44 @@
+//Jack Huggart
+
+package wsd.ass2;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "enquiries", namespace="http://www.rentapplication.uts.edu.au")
+public class Enquiries implements Serializable {
+
+    @XmlElement(name = "enquiry")
+    private ArrayList<Enquiry> list = new ArrayList<Enquiry>();
+
+    public Enquiries() {
+    }
+
+    public ArrayList<Enquiry> getList() {
+        return list;
+    }
+
+    public void addEnquiry(Enquiry enquiry) {
+        list.add(enquiry);
+    }
+
+    public void removeUser(Enquiry enquiry) {
+        list.remove(enquiry);
+    }
+
+    public ArrayList<Enquiry> searchEnquiries(int listingid) {
+        ArrayList<Enquiry> foundenq = new ArrayList<Enquiry>();
+
+        for (Enquiry enquiry : list) {
+            if (enquiry.getListingid() == listingid) {
+                foundenq.add(enquiry);
+            }
+        }
+        return foundenq;
+    }
+}
